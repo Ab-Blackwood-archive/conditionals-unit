b11d5f158b4f18aec1f7bfc45b574c25a38c8d89
Ab-Blackwood-archive <ab061906@wgcloud.org>
2023-11-13 11:44:20 -0600

    finished code

--- animal list.py
+++ animal list.py
@@ -1,5 +1,7 @@
-# guess the animal the proram repeats until the correct animal is guessed
-
+"""""
+guess the animal the proram repeats until the correct animal is guessed when the correct animal is guessed 
+then the program outputs the amount of correct ansewers out of the total attempts
+"""""
 #posible animals
 l= "leopard"  
 z= "zebra"  
@@ -8,15 +10,31 @@ f= "fennic fox"
 o= "ostrich" 
 c= "crocodile" 
  # secret animal
-secrt = l
+secrt = e
 
-#boolian and while loop
-answer = False
-while answer == False :
+#boolian loop and answer counter
+x = 0
+y = 0
+play = True
+#code
+while play :
     ani = input ("what is the secret animal? ")
-    if ani !=secrt:
+    if ani == secrt:
         print("Congratulations! You guessed the animal!")
-        answer == True
-        break
+        y = y + 1
+        x = x + 1
+        q = y / x
+        print("you got the answer right " + str(y) + "/" + str(x) + " times.")
+        print("that's " + str(q) + ".")
+        # continue game or no
+        print("play again? yes or no")
+        if "yes":
+            x = 0
+            y = 0
+            play == True
+        else:
+            play == False
+            break
     else:
+        x = x + 1
         print("Wrong animal, try again.")
